53f32b09f10716cd41e4abba61e927921914244e
Julian Kropp <77173321+julian-kropp-dev@users.noreply.github.com>
2022-09-18 20:36:24 +0200

    Update CLOCCore.ino

--- Code/Final/CLOCCore.ino
+++ Code/Final/CLOCCore.ino
@@ -1,11 +1,3 @@
-//Dear Programmer:
-//When we wrote this code, only god and we knew how it worked.
-//Now, only god knows!
-
-//Therefore, if you are trying to optimize it and you fail (most surely),
-//please increase this counter as a warning for the next person:
-
-//total_hours_wasted_time = 42
 
 // Libraries
 #include <ESP8266WiFi.h>        // Bibliothek, um ESP8266 mit einem Wifi-Netzwerk zu verbinden
